c4f77dbd43d0442a724f6f17c6e9bfe10ce9ff89
sidrxo <101471619+sidrxo@users.noreply.github.com>
2025-12-15 12:50:25 +0000

    Delete android directory

--- android/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_burner_app_BurnerApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.burner.app.BurnerApplication"
-)
-public final class _com_burner_app_BurnerApplication {
-}
